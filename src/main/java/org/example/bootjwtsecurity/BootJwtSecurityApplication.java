199de3b335082ccbe366fe030f258ab61f98dac1
qus0in <qus0in@gmail.com>
2025-04-23 09:34:56 +0900

    Initial commit

---
+++ src/main/java/org/example/bootjwtsecurity/BootJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootjwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJwtSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJwtSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootjwtsecurity/BootJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootjwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJwtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
