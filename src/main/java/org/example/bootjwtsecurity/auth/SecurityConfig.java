96820af0b4abebb36df8f8ef5670054f50450fe2
qus0in <qus0in@gmail.com>
2025-04-23 15:25:46 +0900

    SecurityConfig

--- src/main/java/org/example/bootjwtsecurity/auth/SecurityConfig.java
+++ src/main/java/org/example/bootjwtsecurity/auth/SecurityConfig.java
@@ -1,21 +1,34 @@
 package org.example.bootjwtsecurity.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.example.bootjwtsecurity.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity // Filter용
+@RequiredArgsConstructor // 의존성 주입
 public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
-//                .csrf(csrf -> csrf.disable()) // CSRF 비활성화 2
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**")) // REST API 무시하겠다...
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+//                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**")) // REST API 무시하겠다...
 //                .cors()
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
 auth ->
                     auth
@@ -24,8 +37,30 @@ auth ->
                             .anyRequest()
                                 .authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
-                .logout(Customizer.withDefaults());
+                .authenticationProvider(daoAuthProvider())
+                .addFilterBefore(jwtFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+//                .formLogin(Customizer.withDefaults())
+//                .logout(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // ?
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider);
     }
 }
