cb8af165e97fe670d699ba203aab568f595cca4d
qus0in <qus0in@gmail.com>
2025-04-23 16:26:30 +0900

    푸시.

--- src/main/java/org/example/bootjwtsecurity/controller/TeamController.java
+++ src/main/java/org/example/bootjwtsecurity/controller/TeamController.java
@@ -1,5 +1,9 @@
 package org.example.bootjwtsecurity.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.example.bootjwtsecurity.model.dto.TeamRequestDTO;
 import org.example.bootjwtsecurity.model.entity.Team;
@@ -13,17 +17,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/baseball/teams")
 @RequiredArgsConstructor
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "JWT Bearer token"
+)
 public class TeamController {
 
     private final TeamService teamService;
 
     @GetMapping
+    @Operation(
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
     public ResponseEntity<List<Team>> findAllTeams() {
         List<Team> teamList = teamService.findAllTeams();
         return ResponseEntity.ok(teamList);
     }
 
     @PostMapping
+    @Operation(
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
     public ResponseEntity<Team> createTeam(@RequestBody TeamRequestDTO dto) {
         Team team = teamService.saveTeam(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(team);
